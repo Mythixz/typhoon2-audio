162d85f68f4a046fcc7edc3695428912147d7833
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-12-13 09:41:10 +0700

    feat(generate): change to qwen's conversation format

--- test_generate.py
+++ test_generate.py
@@ -1,26 +1,35 @@
-from typhoon2_audio.modeling_typhoon2audio import Typhoon2AudioForConditionalGeneration, Typhoon2Audio2AudioForConditionalGeneration
+from typhoon2_audio.modeling_typhoon2audio import (
+    Typhoon2AudioForConditionalGeneration,
+    Typhoon2Audio2AudioForConditionalGeneration,
+)
 from transformers import AutoModel
 import soundfile as sf
 import torch
 
-# read wav
-audio, sr = sf.read("tmp/tmp-2860cd0a094b64043226167340af03a3.wav")
-prompt_pattern="<|start_header_id|>system<|end_header_id|>\n\nYou are a helpful female assistant named ไต้ฝุ่น.<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n<Speech><SpeechHere></Speech> {}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+conversation = [
+    {"role": "system", "content": "You are a helpful female assistant named ไต้ฝุ่น."},
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "audio",
+                "audio_url": "tmp/tmp-2860cd0a094b64043226167340af03a3.wav",
+            },
+            {"type": "text", "text": "Transcribe this audio"},
+        ],
+    },
+]
 
 model0 = Typhoon2AudioForConditionalGeneration.from_pretrained(
     "save_weights/llama3.1-typhoon2-audio-8b-instruct-241210/",
-    torch_dtype=torch.float16, # otherwise default to float32
+    torch_dtype=torch.float16,  # otherwise default to float32
 )
 
 model0.to("cuda")
 print("model0.dtype =", model0.dtype)
 print("model0.device =", model0.device)
 
-response0 = model0.generate(
-    audio=audio,
-    prompt="Transcribe this audio",
-    prompt_pattern=prompt_pattern,
-)
+response0 = model0.generate(conversation)
 
 print(response0)
 
@@ -31,20 +40,35 @@ print(response0)
 
 model = AutoModel.from_pretrained(
     "scb10x/llama3.1-typhoon2-audio-8b-instruct-241210",
-    torch_dtype=torch.float16, 
-    trust_remote_code=True
+    torch_dtype=torch.float16,
+    trust_remote_code=True,
 )
 
 model.to("cuda")
 print("model.dtype =", model.dtype)
 print("model.device =", model.device)
 
-x = model.generate(
-    audio=audio,
-    prompt="Respond conversationally to the speech provided in the language it is spoken in.",
-    prompt_pattern=prompt_pattern,
-)
-sf.write("tmp/speechout.wav", x['audio']['array'], x['audio']['sampling_rate'])
+conversation2 = [
+    {"role": "system", "content": "You are a helpful female assistant named ไต้ฝุ่น."},
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "audio",
+                "audio_url": "tmp/tmp-2860cd0a094b64043226167340af03a3.wav",
+            },
+            {
+                "type": "text",
+                "text": "Respond conversationally to the speech provided in the language it is spoken in.",
+            },
+        ],
+    },
+]
+
+x = model.generate(conversation2)
+sf.write("tmp/speechout.wav", x["audio"]["array"], x["audio"]["sampling_rate"])
 print(x)
-import ipdb; ipdb.set_trace()
+import ipdb
+
+ipdb.set_trace()
 print("finish testing generate")
