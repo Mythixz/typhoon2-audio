d2bdbe1d389c5d60bf4a4214dd250afb1967a296
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-12-17 14:03:00 +0700

    feat!(demo): update all demos

--- demo.py
+++ demo.py
@@ -1,22 +1,13 @@
 from typhoon2_audio.modeling_typhoon2audio import (
     DEFAULT_MODEL_SAMPLING_RATE,
-    TensorStreamer,
     Typhoon2Audio2AudioForConditionalGeneration,
 )
 
-from dataclasses import dataclass, field
-from transformers import TextIteratorStreamer
 import shutil
 import os
-from typing import Tuple
 import gradio as gr
+from gradio import ChatMessage
 import numpy as np
-from gradio_webrtc import (
-    ReplyOnPause,
-    WebRTC,
-    AdditionalOutputs,
-    get_twilio_turn_credentials,
-)
 import torch
 import xxhash
 import soundfile as sf
@@ -25,14 +16,7 @@ from datasets import Audio
 
 load_dotenv()
 
-# Twilio TURN credentials configuration
-rtc_configuration = get_twilio_turn_credentials()
-print(rtc_configuration)
-OUT_CHANNELS = 1
-
 OUT_PATH = "out"
-LOADER_STR = "♫"
-yield_index = 0
 
 if os.path.exists(OUT_PATH):
     shutil.rmtree(OUT_PATH)
@@ -45,175 +29,162 @@ model = Typhoon2Audio2AudioForConditionalGeneration.from_pretrained(
 )
 model.to("cuda")
 
+resampler = Audio(sampling_rate=DEFAULT_MODEL_SAMPLING_RATE)
 
-resampler = Audio(sampling_rate=16000)
+theme = gr.themes.Soft(
+    primary_hue=gr.themes.Color(
+        c50="#f7f7fd",
+        c100="#dfdef8",
+        c200="#c4c1f2",
+        c300="#a29eea",
+        c400="#8f8ae6",
+        c500="#756fe0",
+        c600="#635cc1",
+        c700="#4f4a9b",
+        c800="#433f83",
+        c900="#302d5e",
+        c950="#302d5e",
+    ),
+    secondary_hue="rose",
+    neutral_hue="stone",
+)
 
 
-@torch.no_grad()
-def inference(conversations, temperature=0.4, system_prompt=None):
-    updated_conversations = []
+DEFAULT_SYSTEM_PROMPT = "You are a helpful female assistant named ไต้ฝุ่น."
+messages = []
 
-    if system_prompt and system_prompt.strip():
-        updated_conversations.append({"role": "system", "content": system_prompt})
 
-    for conv in conversations:
-        if isinstance(conv["content"], str):
-            updated_conversations.append(
-                {"role": conv["role"], "content": conv["content"]}
-            )
-        else:
-            if isinstance(conv["content"], dict):
-                audio_path = conv["content"]["path"]
-            else:
-                audio_path = conv["content"].file.path
-            updated_conversations.append(
-                {
-                    "role": conv["role"],
-                    "content": [{"type": "audio", "audio_url": audio_path}],
-                }
-            )
-
-    if updated_conversations[-1]["role"] == "assistant":
-        updated_conversations = updated_conversations[:-1]
-
-    streamer = TextIteratorStreamer(
-        model.llama_tokenizer,
-        skip_prompt=True,
-        skip_special_tokens=True,
-        timeout=15,
+def run_inference(audio_input, system_prompt, chat_box):
+    if audio_input is None:
+        return (
+            gr.update(value=None),
+            gr.Button(value="Record Audio to Submit!", interactive=False),
+            "",
+            gr.update(visible=False),
+        )
+    sr, y = audio_input
+    y = y.astype(np.float32)
+    y = y / np.max(np.abs(y))
+    a = resampler.decode_example(
+        resampler.encode_example({"array": y, "sampling_rate": sr})
     )
-    streamer_unit = TensorStreamer(timeout=15)
-    generator = model.generate_stream(
-        conversation=updated_conversations,
-        temperature=temperature,
-        top_p=0.95,
-        max_new_tokens=512,
-        streaming_unit_gen=True,
-        streamer=streamer,
-        streamer_unit=streamer_unit,
-    )
-    for wav_np, text in generator:
-        yield wav_np, text
+    array_hash = str(xxhash.xxh32(a["array"].tostring()).hexdigest())
+    wav_path = f"./tmp/{array_hash}.wav"
+    os.makedirs("tmp", exist_ok=True)
+    if not os.path.exists(wav_path):
+        sf.write(wav_path, a["array"], a["sampling_rate"], format="wav")
 
-
-@dataclass
-class AppState:
-    conversation: list = field(default_factory=list)
-    system_prompt: str = "You are always answer in Thai."
-    stopped: bool = False
-
-
-def response(audio_tuple: Tuple[int, np.ndarray], state: AppState, system_prompt: str):
-    global yield_index
-
-    if system_prompt != state.system_prompt:
-        state.system_prompt = system_prompt
-
-    if not audio_tuple:
-        return AppState(system_prompt=system_prompt)
-
-    sr, audio = audio_tuple
-    file_name = f"out/{xxhash.xxh32(bytes(audio)).hexdigest()}.wav"
-    assert len(audio.shape) == 2
-    sf.write(file_name, audio[0], sr, format="wav")
-
-    state.conversation.append(
-        {"role": "user", "content": {"path": file_name, "mime_type": "audio/wav"}}
+    messages.append(
+        {"role": "user", "content": [{"type": "audio", "audio_url": wav_path}]}
     )
 
-    state.conversation.append(
+    updated_history = [{"role": "system", "content": system_prompt}] + messages
+
+    # Typhoon-Audio model
+    response = model.generate(conversation=updated_history)
+
+    text = response["text"]
+    audio = response["audio"]
+    temp_out_path = f"./tmp/out-{array_hash}.wav"
+    audio_arr = audio["array"].astype(np.float32)
+    sf.write(temp_out_path, audio_arr, audio["sampling_rate"], format="wav")
+
+    output_widget = gr.Audio(temp_out_path)
+    chat_box.append(ChatMessage(role="user", content=gr.Audio(wav_path)))
+    chat_box.append(ChatMessage(role="assistant", content=text))
+    chat_box.append(ChatMessage(role="assistant", content=output_widget))
+
+    messages.append(
         {
             "role": "assistant",
-            "content": LOADER_STR,
+            "content": text,
         }
     )
 
-    decode_sample_rate = DEFAULT_MODEL_SAMPLING_RATE
-    buff = None
-    for wav, text in inference(state.conversation, system_prompt=state.system_prompt):
-        if state.conversation[-1]["content"] == LOADER_STR:
-            state.conversation[-1]["content"] = text
-        else:
-            state.conversation[-1]["content"] = state.conversation[-1]["content"] + text
-        if wav is not None:
-            if buff is None:
-                buff = wav.cpu().numpy()
-            else:
-                buff = np.concatenate((buff, wav.cpu().numpy()))
-        if buff.shape[0] > (decode_sample_rate * 2):
-            yield_index += 1
-            yield (
-                decode_sample_rate,
-                buff.reshape(1, -1),
-                "mono",
-            ), AdditionalOutputs(dict(state=state, chatbot=state.conversation))
-            buff = None
+    return (
+        gr.update(value=None),
+        gr.Button(value="Please upload audio", interactive=False, variant="primary"),
+        chat_box,
+        gr.update(visible=True),
+    )
 
-    if buff is not None and buff.shape[0] > 0:
-        yield (decode_sample_rate, buff.reshape(1, -1), "mono"), AdditionalOutputs(
-            dict(
-                state=AppState(
-                    conversation=state.conversation, system_prompt=state.system_prompt
-                ),
-                chatbot=state.conversation,
-            )
+
+def is_able_to_start(audio_input):
+    if audio_input is not None:
+        return gr.Button(
+            value="Click to run inference", interactive=True, variant="primary"
         )
+    return gr.Button(value="Please upload audio", interactive=False, variant="primary")
 
 
-with gr.Blocks() as omni_demo:
+def gradio_reset_all():
+    messages = []
+
+    return (
+        gr.update(value=None),
+        gr.update(value="Record Audio to Submit!", interactive=False),
+        gr.update(value=None),
+        gr.update(value=DEFAULT_SYSTEM_PROMPT),
+        gr.update(visible=False),
+    )
+
+
+with gr.Blocks(theme=theme) as omni_demo:
     gr.HTML(
         """
     <div style='text-align: center'>
         <h1>
-            Talk To Typhoon 2 Audio (Powered by WebRTC ⚡️)
+            Converse With Typhoon2 Audio
         </h1>
-        <p>
-            Each conversation is limited to 60 seconds. Once the time limit is up you can rejoin the conversation.
-        </p>
     </div>
     """
     )
-    with gr.Column():
-        with gr.Row():
-            system_prompt = gr.Textbox(
-                label="System Prompt",
-                value="You are always answer in Thai.",
-                info="Customize the system prompt to guide the AI's behavior",
-            )
-        with gr.Group():
-            webrtc = WebRTC(
-                label="Stream",
-                rtc_configuration=rtc_configuration,
-                mode="send-receive",
-                modality="audio",
-            )
-        with gr.Row():
-            chatbot = gr.Chatbot(label="Conversation", type="messages")
-        state = gr.State(value=AppState())
-        webrtc.stream(
-            fn=ReplyOnPause(
-                response,
-                output_sample_rate=DEFAULT_MODEL_SAMPLING_RATE,
-                input_sample_rate=DEFAULT_MODEL_SAMPLING_RATE,
-                output_frame_size=480,
-            ),
-            inputs=[webrtc, state, system_prompt],
-            outputs=[webrtc],
-            time_limit=60,
-        )
+    with gr.Row():
+        with gr.Column(scale=4):
+            with gr.Row():
+                system_prompt = gr.Textbox(
+                    value=DEFAULT_SYSTEM_PROMPT,
+                    label="System Prompt",
+                    placeholder="You can control the model's behavior by specifying the system prompt here.",
+                )
+            with gr.Row():
+                cur_dir = os.path.dirname(os.path.abspath(__file__))
+                audio_input = gr.Audio(
+                    sources=["upload", "microphone"],
+                    streaming=False,
+                    label="Microphone Input",
+                )
+            with gr.Row():
+                submit_button = gr.Button(
+                    value="Record Audio to Submit!", interactive=False
+                )
 
-        def update_state(response):
-            state = response["state"]
-            chatbot = response["chatbot"]
-            return state, chatbot
+            with gr.Row():
+                clear_button = gr.Button(value="Clear the recording", visible=False)
+        with gr.Column(scale=8):
+            with gr.Row():
+                chat_box = gr.Chatbot(type="messages", height=1000)
 
-        webrtc.on_additional_outputs(
-            update_state,
-            inputs=[],
-            outputs=[state, chatbot],
-            queue=False,
-            show_progress="hidden",
-        )
+            audio_input.change(is_able_to_start, [audio_input], [submit_button])
+
+            submit_button.click(
+                fn=run_inference,
+                inputs=[audio_input, system_prompt, chat_box],
+                outputs=[audio_input, submit_button, chat_box, clear_button],
+            )
+
+            clear_button.click(
+                fn=gradio_reset_all,
+                inputs=[],
+                outputs=[
+                    audio_input,
+                    submit_button,
+                    chat_box,
+                    system_prompt,
+                    clear_button,
+                ],
+                queue=False,
+            )
 
 
 # --- Processing Demo ---
@@ -278,7 +249,6 @@ def run_inference(audio_input, text_prompt):
             }
         ]
     )
-    print(typhoon_output["text"])
     yield (
         gr.Button(value="Click to run inference", interactive=True, variant="primary"),
         gr.Textbox(
@@ -290,12 +260,12 @@ def run_inference(audio_input, text_prompt):
 
 
 text_input = ""
-with gr.Blocks() as processing_demo:
+with gr.Blocks(theme=theme) as processing_demo:
     gr.HTML(
         """
     <div style='text-align: center'>
         <h1>
-            Audio Processing With Typhoon 2 Audio
+            Audio Processing With Typhoon2 Audio
         </h1>
     </div>
     """
@@ -367,12 +337,12 @@ def generate_speech(text: str):
 
 
 default_text = ""
-with gr.Blocks() as tts_demo:
+with gr.Blocks(theme=theme) as tts_demo:
     gr.HTML(
         """
     <div style='text-align: center'>
         <h1>
-            Text-to-Speech Generation With Typhoon 2 Audio
+            Text-to-Speech Generation With Typhoon2 Audio
         </h1>
     </div>
     """
@@ -390,11 +360,30 @@ with gr.Blocks() as tts_demo:
         flagging_mode="never",
     )
 
-demo = gr.TabbedInterface(
-    [omni_demo, processing_demo, tts_demo],
-    ["Live Conversation Demo", "Audio Processing Demo", "Text-to-Speech Demo"],
-    title="Typhoon 2 Audio - Demo",
-)
+with gr.Blocks(
+    theme=theme, title="🌪️ Typhoon2 Audio: Native Thai End-to-End Audio-Language Model"
+) as demo:
+    gr.Markdown(
+        """<center><h1>🌪️ Typhoon2 Audio: Native Thai End-to-End Audio-Language Model</h1></center><br/>
+        Typhoon2-Audio understands **Speech** as well as **Audio** events, and it responds in Thai and English texts
+- Its capabilities include transcription, speech translation, speech instruction following, spoken-document Q&A, audio captioning, etc.
+- The research preview model may not work well on long audio clips, particularly those exceeding 30 seconds. Text prompt can be empty for speech instruction following."""
+    )
+    gr.TabbedInterface(
+        [omni_demo, processing_demo, tts_demo],
+        ["Conversation Demo", "Audio Processing Demo", "Text-to-Speech Demo"],
+        theme=theme,
+    )
+    gr.Markdown(
+        """### Disclaimer
+The responses generated by this Artificial Intelligence (AI) system are autonomously constructed and do not necessarily reflect the views or positions of the developing organizations, their affiliates, or any of their employees. These AI-generated responses do not represent those of the organizations. The organizations do not endorse, support, sanction, encourage, verify, or agree with the comments, opinions, or statements generated by this AI. The information produced by this AI is not intended to malign any religion, ethnic group, club, organization, company, individual, anyone, or anything. It is not the intent of the organizations to malign any group or individual. The AI operates based on its programming and training data and its responses should not be interpreted as the explicit intent or opinion of the organizations.
+
+### Terms of use
+By using this service, users are required to agree to the following terms: The service is a research preview intended for non-commercial use only. It only provides limited safety measures and may generate offensive content. It must not be used for any illegal, harmful, violent, racist, or sexual purposes. Audio language models are prone to hallucinations to a greater extent compared to text-only LLMs. A known limitation is that the current version cannot handle audio longer than 30 seconds. The service may collect user dialogue data for future research.
+
+### License
+This project utilizes certain datasets and checkpoints that are subject to their respective original licenses. Users must comply with all terms and conditions of these original licenses. The content of this project itself is licensed under the Apache license 2.0."""
+    )
 
 demo.queue()
 demo.launch(ssr_mode=False)
