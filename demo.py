e7c43edf7c78b39a1ecab3043382ba12cedcbe8c
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-12-19 06:59:43 +0700

    feat: support system prompt in audio processing tab

--- demo.py
+++ demo.py
@@ -3,7 +3,6 @@ from typhoon2_audio.modeling_typhoon2audio import (
     Typhoon2Audio2AudioForConditionalGeneration,
 )
 
-import shutil
 import os
 import gradio as gr
 from gradio import ChatMessage
@@ -76,7 +75,11 @@ def run_inference(audio_input, system_prompt, chat_box):
         {"role": "user", "content": [{"type": "audio", "audio_url": wav_path}]}
     )
 
-    updated_history = [{"role": "system", "content": system_prompt}] + messages
+    updated_history = (
+        [{"role": "system", "content": system_prompt}] + messages
+        if system_prompt and system_prompt.strip() != ""
+        else messages
+    )
 
     # Typhoon-Audio model
     response = model.generate(conversation=updated_history)
@@ -195,6 +198,7 @@ def is_able_to_start(audio_input):
 
 
 def gradio_reset_all():
+    sys_prompt = DEFAULT_SYSTEM_PROMPT
     return (
         gr.update(value=None),
         gr.update(
@@ -208,7 +212,7 @@ def gradio_reset_all():
     )
 
 
-def run_inference(audio_input, text_prompt):
+def run_inference(audio_input, text_prompt, sys_prompt):
     yield (
         gr.Button(
             value="Waiting in queue for GPU time...",
@@ -236,17 +240,24 @@ def run_inference(audio_input, text_prompt):
     if not os.path.exists(wav_path):
         sf.write(wav_path, a["array"], a["sampling_rate"], format="wav")
 
-    typhoon_output = model.generate(
-        conversation=[
+    conversation = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": text_prompt},
+                {"type": "audio", "audio_url": wav_path},
+            ],
+        }
+    ]
+
+    if sys_prompt and sys_prompt.strip() != "":
+        conversation = [
             {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": text_prompt},
-                    {"type": "audio", "audio_url": wav_path},
-                ],
+                "role": "system",
+                "content": sys_prompt,
             }
-        ]
-    )
+        ] + conversation
+    typhoon_output = model.generate(conversation=conversation)
     yield (
         gr.Button(value="Click to run inference", interactive=True, variant="primary"),
         gr.Textbox(
@@ -279,7 +290,12 @@ with gr.Blocks(theme=theme) as processing_demo:
                 )
 
         with gr.Column(scale=8):
-            with gr.Row():
+            with gr.Column():
+                sys_prompt = gr.Textbox(
+                    value=DEFAULT_SYSTEM_PROMPT,
+                    label="System Prompt",
+                    placeholder="You can control the model's behavior by specifying the system prompt here.",
+                )
                 text_input = gr.Textbox(
                     value="",
                     label="Text Prompt",
@@ -300,7 +316,7 @@ with gr.Blocks(theme=theme) as processing_demo:
 
             submit_button.click(
                 fn=run_inference,
-                inputs=[audio_input, text_input],
+                inputs=[audio_input, text_input, sys_prompt],
                 outputs=[submit_button, output_ta, clear_button],
             )
 
@@ -384,4 +400,4 @@ This project utilizes certain datasets and checkpoints that are subject to their
     )
 
 demo.queue()
-demo.launch(ssr_mode=False)
+demo.launch(ssr_mode=False, share=True)
